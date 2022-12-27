ef7264083f19bab8f01cd16f0ab02642c5da3fb7
akshay <akshaymali359@mail.com>
2022-12-27 11:03:32 +0530

    initial commit

---
+++ src/com/test/Rectangle.java
@@ -0,0 +1,15 @@
+package com.test;
+public class Rectangle {
+	
+	public static void main(String args[])
+	{
+		float l=10, b=8;
+		
+		double area=l*b;
+		double perimeter= 2*(l+b);
+		
+		System.out.println("Area="+ area);
+		System.out.println("Perimeter="+perimeter);
+	}
+
+}
---
+++ src/com/test/Rectangle1.java
@@ -0,0 +1,25 @@
+package com.test;
+import java.util.Scanner;
+
+public class Rectangle1 {
+	
+	public static void main(String args[])
+	{
+		float l, b;
+		Scanner scan=new Scanner(System.in);
+		//system.in - input stream
+		
+		System.out.println("Enter the value of l:");
+		l=scan.nextInt();
+		
+		System.out.println("Enter the value of b:");
+		b=scan.nextInt();
+		
+		double area=l*b;
+		double perimeter= 2*(l+b);
+		
+		System.out.println("Area="+ area);
+		System.out.println("Perimeter="+perimeter);
+	}
+
+}
